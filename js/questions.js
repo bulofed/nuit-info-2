72905fd960472d5617d5526f96572c23c187263a
BUTOR Loic <loic.butor@etu.univ-littoral.fr>
2023-12-08 05:23:43 +0100

    quiz

---
+++ js/questions.js
@@ -0,0 +1,39 @@
+function questions(numero)
+{
+	var chaine="";
+	switch(numero)
+	{
+		case 1:
+		chaine="Qu’est ce qu’une IST ?*ski*skiing*to skiing*to skying*2";
+		break;
+		case 2:
+		chaine="D’après vous combien de personnes sont touchées par des IST ?*make*do*have*be*3";
+		break;
+		case 3:
+		chaine="Comment éviter cela ?*noun*verb*adverb*adjective*4";
+		break;
+		case 4:
+		chaine="Peut-on attraper une IST en pratiquant une fellation ?*take*make*have*do*1";
+		break;
+		case 5:
+		chaine="Vous pratiquez vos fellations avec ou sans préservatif ?*come to*come from*come back from*come to from*2";
+		break;
+		case 6:
+		chaine="J’ai un micro pénis, comment je fais ?*bring*taken*brought*took*3";
+		break;
+		case 7:
+		chaine="Peut-on attraper des IST en empruntant le chemin boueux ?*get up*got over*got*got up*4";
+		break;
+		case 8:
+		chaine="Les objets peuvent-ils transmettre des IST ?*asked*said*told*spoke*1";
+		break;
+		case 9:
+		chaine=".*at*to*in*on*3";
+		break;
+		case 10:
+		chaine=".*althought*but*even*because*4";
+		break;
+	}
+	
+	return chaine;
+}
