533c5fdf89392c142f54caacb63fbc39209b1d29
BUTOR Loic <loic.butor@etu.univ-littoral.fr>
2023-12-08 05:55:49 +0100

    finish

--- js/questions.js
+++ js/questions.js
@@ -4,34 +4,34 @@ function questions(numero)
 	switch(numero)
 	{
 		case 1:
-		chaine="Qu’est ce qu’une IST ?*ski*skiing*to skiing*to skying*2";
+		chaine="Quelle est l'une des méthodes efficaces pour lutter contre le gaspillage alimentaire?*Jeter les restes alimentaires dans la poubelle ordinaire*Acheter en grande quantité pour bénéficier de réductions*Planifier ses repas, conserver et réutiliser les restes de manière créative*Ignorer la date de péremption des produits alimentaires*3";
 		break;
 		case 2:
-		chaine="D’après vous combien de personnes sont touchées par des IST ?*make*do*have*be*3";
+		chaine="Comment pouvez-vous contribuer à la conservation de l'eau dans votre vie quotidienne?*Prendre des bains*Arroser le jardin pendant les heures les plus chaudes de la journée*Réparer rapidement les fuites d'eau*Laver la vaisselle avec l'eau qui a été utilisée pour cuisiner*3";
 		break;
 		case 3:
-		chaine="Comment éviter cela ?*noun*verb*adverb*adjective*4";
+		chaine="Qu'est-ce qu'une action responsable en matière de consommation numérique?*Télécharger et stocker autant de fichiers que possible*Ignorer les mises à jour logicielles*Éteindre les appareils électroniques lorsqu'ils ne sont pas utilisés*Utiliser des mots de passe faciles à deviner pour faciliter l'accès*3";
 		break;
 		case 4:
-		chaine="Peut-on attraper une IST en pratiquant une fellation ?*take*make*have*do*1";
+		chaine="Comment pouvez-vous adopter une conduite éco-responsable?*Utiliser fréquemment la climatisation de la voiture*Faire du covoiturage ou utiliser les transports en commun*Laisser le moteur de la voiture tourner pendant de longues périodes à l'arrêt*Jeter les déchets par la fenêtre pendant la conduite*2";
 		break;
 		case 5:
-		chaine="Vous pratiquez vos fellations avec ou sans préservatif ?*come to*come from*come back from*come to from*2";
+		chaine="Quel est l'impact environnemental de la surconsommation numérique?*Aucun impact, car les appareils sont petits*La production accrue de déchets électroniques et la consommation d'énergie*L'amélioration de la qualité de l'air*L'augmentation des prix*2";
 		break;
 		case 6:
-		chaine="J’ai un micro pénis, comment je fais ?*bring*taken*brought*took*3";
+		chaine="Quelle est une méthode écologique pour disposer des déchets électroniques, tels que les vieux téléphones portables?*Les jeter dans la poubelle ordinaire*Les recycler dans des centres spécialisés*Les enterrer dans le jardin pour les décomposer naturellement*Les brûler pour en réduire le volume*2";
 		break;
 		case 7:
-		chaine="Peut-on attraper des IST en empruntant le chemin boueux ?*get up*got over*got*got up*4";
+		chaine="Pourquoi est-il important de réduire la consommation de viande dans une perspective écologique?*La viande est trop chère*La production de viande contribue à la déforestation et à l'émission de gaz à effet de serre*Les animaux de ferme sont des espèces en voie de disparition*La viande contient des produits chimiques nocifs pour l'environnement*2";
 		break;
 		case 8:
-		chaine="Les objets peuvent-ils transmettre des IST ?*asked*said*told*spoke*1";
+		chaine="Quel est le principal avantage des emballages alimentaires réutilisables par rapport aux emballages jetables?*Ils sont plus colorés et attrayants*Ils contribuent à réduire la production de déchets*Ils sont moins chers à produire*Ils peuvent être utilisés une seule fois pour des occasions spéciales*2";
 		break;
 		case 9:
-		chaine=".*at*to*in*on*3";
+		chaine="Comment la déforestation contribue-t-elle au changement climatique?*Elle crée des zones plus fraîches avec une meilleure qualité d'air*Elle augmente la biodiversité en éliminant les espèces indésirables*Elle libère du dioxyde de carbone dans l'atmosphère*Elle n'a aucun impact sur le climat*3";
 		break;
 		case 10:
-		chaine=".*althought*but*even*because*4";
+		chaine="Qu'est-ce que le 'commerce équitable' en matière de production alimentaire?*Un type de commerce où les prix sont fixés arbitrairement*Un modèle commercial visant à garantir des conditions justes pour les producteurs, en particulier dans les pays en développement*Une stratégie visant à maximiser les profits des grandes entreprises*Une approche qui favorise la surproduction alimentaire sans se soucier des conséquences environnementales*2";
 		break;
 	}
 	
